e5ff0f29c0b3959e9a1649e98a0070b8a79b3ace
Allan <allandiascavalcante@gmail.com>
2023-06-06 11:14:45 -0300

    Update ex011.py

--- Exercicios/ex011.py
+++ Exercicios/ex011.py
@@ -1,4 +1,3 @@
 la = float(input('Digite a largura da parede: '))
 al = float(input('Digite a altura da parede: '))
-ar = la * al
-print('Para pintar {:.2f}M² serão necessarios {:.2f}l de tinta.'.format(ar, ar/2))
+print('Para pintar {:.2f}M² serão necessarios {:.2f}l de tinta.'.format(la*al, (la*al)/2))
