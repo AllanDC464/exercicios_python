eed8711615503acad018915b0c12f7d60619624b
Allan <allandiascavalcante@gmail.com>
2023-06-06 12:24:55 -0300

    Exercícios extras

---
+++ Exercicios/ex014.py
@@ -0,0 +1,3 @@
+temp = float(input('Qual a temperatura atual em graus celsius: '))
+print('{}°C corresponde a {}°F!'.format(temp, temp * 1.8 + 32))
+print('{}°C corresponde a {}°K!'.format(temp, temp + 273.15))
---
+++ Exercicios/ex015.py
@@ -0,0 +1,3 @@
+da = int(input('Quantos dias alugados? '))
+kr = float(input('Quantos Km rodados? '))
+print('O total a pagar é de R${:.2f}'.format((da * 60) + (kr * 0.15)))
