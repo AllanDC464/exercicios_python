5a38be659ea342d5ef16d181206edda36661664c
Allan <allandiascavalcante@gmail.com>
2023-06-06 11:30:44 -0300

    Update ex013.py

--- Exercicios/ex013.py
+++ Exercicios/ex013.py
@@ -1,3 +1,3 @@
-sal = float(input('Digite se salario atual R$ '))
+sal = float(input('Digite seu salario atual R$'))
 nsal = (15 * sal)/100
 print('Voce obteve um aumento de salario de R${:.2f}, e agora recebe R${:.2f}!'.format(nsal, sal+nsal))
