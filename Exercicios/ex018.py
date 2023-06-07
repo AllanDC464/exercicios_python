8473cb308f16228355d1a3d9c53796e4419e8273
Allan <allandiascavalcante@gmail.com>
2023-06-07 08:52:14 -0300

    Update ex018.py

--- Exercicios/ex018.py
+++ Exercicios/ex018.py
@@ -1,3 +1,3 @@
-from math import sin, cos, tan
+from math import sin, cos, tan, radians
 ang = int(input('Digite um ângulo: '))
-print('Ângulo = {}° \nSeno = {:.2f}° \nCosseno = {:.2f}° \nTangente = {:.2f}°'.format(ang, sin(ang), cos(ang), tan(ang)))
+print('Ângulo = {}° \nSeno = {:.2f} \nCosseno = {:.2f} \nTangente = {:.2f}'.format(ang, sin(radians(ang)), cos(radians(ang)), tan(radians(ang))))
