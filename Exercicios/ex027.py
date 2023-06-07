349bdd843a296d78c56c7736b429a6b462a0ff86
Allan <allandiascavalcante@gmail.com>
2023-06-07 16:13:28 -0300

    Update ex027.py

--- Exercicios/ex027.py
+++ Exercicios/ex027.py
@@ -1,4 +1,4 @@
-nome = input('Digite seu nome completo: ')
+nome = input('Digite seu nome completo: ').strip()
 nomes = nome.split()
 print('Primeiro nome: {}'.format(nomes[0]))
 print('Último nome: {}'.format(nomes[-1]))
