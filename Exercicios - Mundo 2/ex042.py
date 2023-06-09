e35594f3f4c52ee0b9647244c8e6c25355b203ec
Allan <allandiascavalcante@gmail.com>
2023-06-09 15:10:50 -0300

    Update ex042.py

--- Exercicios - Mundo 2/ex042.py
+++ Exercicios - Mundo 2/ex042.py
@@ -2,11 +2,12 @@ r1 = float(input('1º segmento: '))
 r2 = float(input('2º segmento: '))
 r3 = float(input('3º segmento: '))
 if r1 + r2 > r3 and r1 + r3 > r2 and r2 + r3 > r1:
+    print('Os segmentos acima formam um triangulo ', end='')
     if r1 == r2 and r2 == r3:
-        print('Os segmentos acima formam um triangulo equilátero!')
+        print('equilátero!')
     elif r1 == r2 or r1 == r3 or r2 == r3:
-        print('Os segmentos acima formam um triangulo isóceles!')
+        print('isóceles!')
     else:
-        print('Os segmentos acima formam um triangulo escaleno!')
+        print('escaleno!')
 else:
     print('Os segmentos acima não podem formar um triangulo!')
