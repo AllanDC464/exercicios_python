2fabf92be2491038b956001cd06597c73387af43
Allan <allandiascavalcante@gmail.com>
2023-06-09 15:03:08 -0300

    Update ex041.py

--- Exercicios - Mundo 2/ex041.py
+++ Exercicios - Mundo 2/ex041.py
@@ -4,13 +4,18 @@ ano_atual = date.today().year
 idade = ano_atual - ano_nasc
 if idade < 0:
     print('\033[1;30;41mIdade invalida!\033[m')
-elif idade > 20:
+elif idade > 25:
+    print('O atleta tem {} anos.'.format(idade))
     print('O atleta esta na categoria \033[34mMASTER\033[m!')
 elif idade > 19:
+    print('O atleta tem {} anos.'.format(idade))
     print('O atleta esta na categoria \033[34mSÊNIOR\033[m!')
 elif idade > 14:
+    print('O atleta tem {} anos.'.format(idade))
     print('O atleta esta na categoria \033[34mJUNIOR\033[m!')
 elif idade > 9:
+    print('O atleta tem {} anos.'.format(idade))
     print('O atleta esta na categoria \033[34mINFANTIL\033[m!')
 else:
+    print('O atleta tem {} anos.'.format(idade))
     print('O atleta esta na categoria \033[34mMIRIM\033[m!')
