449f1cd77c85188b3422fa261e80f57caeb488bc
Allan <allandiascavalcante@gmail.com>
2023-06-09 16:32:38 -0300

    Update ex045.py

--- Exercicios - Mundo 2/ex045.py
+++ Exercicios - Mundo 2/ex045.py
@@ -1,20 +1,33 @@
 from random import randint
-player = int(input('[1] Pedra \n[2] Papel \n[3] Tesoura \nEscolha uma opção: '))
-pc = randint(1, 3)
-if player == 1 and pc == 2:
-    print('Você perdeu')
-elif player == 1 and pc ==3:
-    print('Você ganhou')
-elif player == 2 and pc == 1:
-    print('Você ganhou')
-elif player == 2 and pc == 3:
-    print('Você perdeu')
-elif player == 3 and pc == 1:
-    print('Você perdeu')
-elif player == 3 and pc == 2:
-    print('Você ganhou')
-elif player == pc:
-    print('Empatou')
-else:
-    print('Opção invalida! \nTente outra opção!')
-    
+itens = ('Pedra', 'Papel', 'Tesoura')
+pc = randint(0, 2)
+Jogador = int(input('[1] Pedra \n[2] Papel \n[3] Tesoura \nQual sua jogada? '))
+print('Computador jogou {}'.format(itens[pc]))
+print('Jogador jogou {}'.format(itens[Jogador]))
+if pc == 0:
+    if Jogador == 0:
+        print('EMPATE')
+    elif Jogador == 1: 
+        print('Jogador VENCEU')
+    elif Jogador == 2:
+        print('Computador VENCEU')
+    else:
+        print('Jogada INVALIDA')
+elif pc == 1:
+    if Jogador == 0:
+        print('Computador VENCE')
+    elif Jogador == 1:
+        print('EMPATE')
+    elif Jogador == 2:
+        print('Jogador VENCE')
+    else:
+        print('Jogada INVALIDA')
+elif pc == 2:
+    if Jogador == 0:
+        print('Jogador VENCE')
+    elif Jogador == 1:
+        print('Computador VENCE')
+    elif Jogador == 2:
+        print('EMPATE')
+    else:
+        print('Jogada INVALIDA')
