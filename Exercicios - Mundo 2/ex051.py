95c023f2df03d936aabc514b7ed95f6aab2ee240
Allan <allandiascavalcante@gmail.com>
2023-06-12 19:07:20 -0300

    Update ex051.py

--- Exercicios - Mundo 2/ex051.py
+++ Exercicios - Mundo 2/ex051.py
@@ -1,6 +1,9 @@
+print('=' * 25)
+print('10 TERMOS DE UMA PA'.center(25))
+print('=' * 25)
 primter = int(input('1º termo da sua PA: '))
 razao = int(input('Razão da sua PA: '))
-print(primter)
-for c in range(0, 9):
+for c in range(0, 10):
+    print(primter, end=' → ')
     primter += razao
-    print(primter)
+print('ACABOU!')
