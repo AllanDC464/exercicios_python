485f0cd08e6c528a881dc6c0bc77362ade80972a
Allan <allandiascavalcante@gmail.com>
2023-06-12 18:46:32 -0300

    Update ex050.py

--- Exercicios - Mundo 2/ex050.py
+++ Exercicios - Mundo 2/ex050.py
@@ -1,6 +1,8 @@
 s = 0
+cont = 0
 for c in range(1, 7):
     num = int(input('Digite o {}º valor: '.format(c)))
     if num % 2 == 0:
         s += num
-print('A soma dos valores pares digitados foi de {}'.format(s))
+        cont += 1
+print('Você informou \033[4;32m{}\033[m valores pares e a soma entre eles é \033[4;32m{}\033[m'.format(cont, s))
