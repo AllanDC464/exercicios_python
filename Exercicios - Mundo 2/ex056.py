bb625035ff14353c3832eac9960a4bfb51becf64
Allan <allandiascavalcante@gmail.com>
2023-06-12 20:26:57 -0300

    Update ex056.py

--- Exercicios - Mundo 2/ex056.py
+++ Exercicios - Mundo 2/ex056.py
@@ -1,21 +1,22 @@
 m = 0
 nmv = ''
-idmv = ''
+idmv = 0
 mnv = 0
 for c in range(1, 5):
+    print('▬▬▬▬▬▬▬ {}° PESSOA ▬▬▬▬▬▬▬'.format(c))
     nome = str(input('Digite o nome: '))
     idade = int(input('Digite a idade: '))
-    sexo = int(input('[ 1 ] HOMEM \n[ 2 ] MULHER \nSua opção: '))
+    sexo = str(input('Sexo [M/F]: '))
     m += idade
     med = m / 4
-    if c == 1:
+    if c == 1 and sexo in 'Mm':
         nmv = nome
         idmv = idade
-    else:
-        if sexo == 1 and idade > idmv:
+    if sexo in 'Mm' and idade > idmv:
             nmv = nome
-        if sexo == 2 and idade < 20:
+            idmv = idade
+    if sexo in 'Ff' and idade < 20:
             mnv += 1
-print('A média de idade do grupo é de {:.0f} anos.'.format(med))
-print('O nome do homem mais velho é {}.'.format(nmv))
+print('A média de idade do grupo é de {} anos.'.format(med))
+print('A idade do homem mais velho é {} e ele se chama {}.'.format(idmv, nmv))
 print('A {} mulheres com menos de 20 anos.'.format(mnv))
