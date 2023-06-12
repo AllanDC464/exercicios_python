40184b53206c362eaf9d80ed4353cef169055cfd
Allan <allandiascavalcante@gmail.com>
2023-06-12 19:46:53 -0300

    Update ex053.py

--- Exercicios - Mundo 2/ex053.py
+++ Exercicios - Mundo 2/ex053.py
@@ -1,7 +1,7 @@
-frase = str(input('Digite uma frase: '))
-frasem = frase.strip().lower().replace(' ', '')
+frase = str(input('Digite uma frase: ')).lower()
+frasem = frase.strip().replace(' ', '')
 inve = ''.join(reversed(frasem))
 if frasem == inve:
-    print('A frase \033[4;32m{}\033[m é um palindromo.'.format(frase))
+    print('A frase \033[4;32m{}\033[m é um palindromo, e ao contrario fica \033[4;32m{}\033[m.'.format(frase, inve))
 else:
-    print('A frase \033[4;31m{}\033[m não é um palindromo.'.format(frase))
+    print('A frase \033[4;31m{}\033[m não é um palindromo, e ao contrario fica \033[4;31m{}\033[m.'.format(frase, inve))
