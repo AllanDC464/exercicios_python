9a79967472620e5562edc4827a449b8dcf7bdfc6
Allan <allandiascavalcante@gmail.com>
2023-06-18 14:56:19 -0300

    Update ex058.py

--- Exercicios - Mundo 2/ex058.py
+++ Exercicios - Mundo 2/ex058.py
@@ -3,22 +3,16 @@ from time import sleep
 print('\033[33m-=-\033[m' * 10)
 print('\033[1;34m... Pensando em um número ...\033[m')
 print('\033[33m-=-\033[m' * 10)
-rand = randint(0, 11)
+pc = randint(0, 10)
 sleep(2)
-num = int(input('\033[1;31mPensei em um número entre 0 e 5, adivinhe se puder: \033[m'))
-cont = 1
-while num != rand:
-    print('Você errou, tente novamente!')
-    num = int(input('\033[1;31mPensei em um número entre 0 e 5, adivinhe se puder: \033[m'))
+print('\033[1;31mPensei em um número entre 0 e 10, adivinhe se puder! \033[m')
+cont = 0
+player = ''
+while player != pc:
     cont += 1
+    player = int(input('\033[1;32mQual seu palpite?\033[m '))
+    if player < pc:
+        print('Mais... Tente mais uma vez.')
+    elif player > pc:
+        print('Menos... Tente mais uma vez.')
 print('\033[32mVocê acertou!\033[m \nForam necessárias \033[1;4m{} tentativas.\033[m'.format(cont))
-
-
-
-
-"""if num == rand:
-    sleep(1)
-    print('\033[1;32mVocê acertou!\033[m')
-else:
-    sleep(1)
-    print('\033[1;31mVocê errou\033[m, o número em que pensei foi \033[1;32m{}\033[m e não \033[1;31m{}\033[m'.format(rand, num))"""
