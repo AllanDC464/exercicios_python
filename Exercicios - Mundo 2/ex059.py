8e21336937c0720fd8d78db8f681e50cf00de4dc
Allan <allandiascavalcante@gmail.com>
2023-06-19 19:09:05 -0300

    Update ex059.py

--- Exercicios - Mundo 2/ex059.py
+++ Exercicios - Mundo 2/ex059.py
@@ -1,18 +1,35 @@
+from time import sleep
 vm = 0
 num1 = int(input('Digite um valor: '))
 num2 = int(input('Digite um valor: '))
 while vm != 5:
-    vm = int(input('[1]Somar \n[2]Multiplicar \n[3]Maior \n[4]Novos números \n[5]Sair do programa \nOPÇÂO: '))
-    if vm == 4:
-        num1 = int(input('Digite um valor: '))
-        num2 = int(input('Digite um valor: '))
-        vm = int(input('[1]Somar \n[2]Multiplicar \n[3]Maior \n[4]Novos números \n[5]Sair do programa'))
-    elif vm == 1:
-        print('A soma entre {} e {} é {}'.format(num1, num2, num1 + num2))
+    vm = int(input('[1]Somar \n[2]Multiplicar \n[3]Maior \n[4]Novos números \n[5]Sair do programa \n→ OPÇÂO: '))
+    if vm == 1:
+        print('=-=' * 10)
+        print('\033[32mA soma entre {} e {} é {}\033[m'.format(num1, num2, num1 + num2))
+        print('=-=' * 10)
     elif vm == 2:
-        print('O produto entre {} e {} é {}'.format(num1, num2, num1 * num2))
+        print('=-=' * 10)
+        print('\033[32mO produto entre {} e {} é {}\033[m'.format(num1, num2, num1 * num2))
+        print('=-=' * 10)
     elif vm == 3:
         if num1 > num2:
-            print('O número {} é maior que {}'.format(num1, num2))
+            print('=-=' * 10)
+            print('\033[32mO número {} é maior que {}\033[m'.format(num1, num2))
+            print('=-=' * 10)
         else:
-            print('O número {} é maior que {}'.format(num2, num1))
+            print('=-=' * 10)
+            print('\033[32mO número {} é maior que {}\033[m'.format(num2, num1))
+            print('=-=' * 10)
+    elif vm == 4:
+            print('Informe novamente os valores.')
+            num1 = int(input('Digite um valor: '))
+            num2 = int(input('Digite um valor: '))
+    elif vm == 5:
+        print('\033[4;32mFINALIZANDO...\033[m')
+        sleep(2)
+    elif vm > 5:
+        print('=-=' * 10)
+        print('\033[31mOPÇÃO INVÁLIDA!\033[m')
+        print('=-=' * 10)
+print('\033[4;32mFIM DO PROGRAMA! VOLTE SEMPRE!\033[m')
