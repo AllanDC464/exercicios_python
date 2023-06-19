2c608710adaab43fef2eaeaaddb551121c531006
Allan <allandiascavalcante@gmail.com>
2023-06-19 19:38:19 -0300

    Update ex060.py

--- Exercicios - Mundo 2/ex060.py
+++ Exercicios - Mundo 2/ex060.py
@@ -1,19 +1,32 @@
-num = int(input('Digite um número: '))
+num = int(input('Digite um número para calcular o fatorial: '))
 n = num
 numin = num
-
+print('Calculando {}! = '.format(num),end='')
 while n != 1:
-    n = n - 1
-    num = num * n
-print('O fatorial de {} é {}'.format(numin, num))
+    print(n, end=' x ')
+    n -= 1
+    num *= n
+print('1 = {}'.format(num))
+
+
 
 # ↓ ↓ ↓ Resolução com for ↓ ↓ ↓
 
-"""num = int(input('Digite um número: '))
+"""num = int(input('Digite um número para calcular o fatorial: '))
 n = num
 numin = num
-
+print('Calculando {}! = '.format(num),end='')
 for c in range(1, num):
+    print(n, end=' x ')
     n -= 1
-    num = num * n
-print('O fatorial de {} é {}'.format(numin, num))"""
+    num *= n
+print('1 = {}'.format(num))"""
+
+
+
+# ↓ ↓ ↓ Resolução com método ↓ ↓ ↓
+
+"""from math import factorial
+n = int(input('Digite um número para calcular o fatorial: '))
+f = factorial(n)
+print('O fatorial de {} é {}.'.format(n, f))"""
