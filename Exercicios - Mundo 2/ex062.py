e857ddfdb82b63cfc0512ecfc586896e1d169816
Allan <allandiascavalcante@gmail.com>
2023-06-19 21:05:04 -0300

    Update ex062.py

--- Exercicios - Mundo 2/ex062.py
+++ Exercicios - Mundo 2/ex062.py
@@ -1,22 +1,39 @@
-primter = int(input('1° termo da PA: '))
+from time import sleep
+
+print('\033[32m-=-\033[m' * 7)
+print('GERADOR DE PA'.center(21))
+print('\033[32m-=-\033[m' * 7)
+
+print('')
+termo = int(input('1° termo da PA: '))
 razao = int(input('Razão da PA: '))
+print('')
 
 c = 0
 while c < 10:
     c += 1
-    print(primter, end=' → ')
-    primter += razao
-print('ACABOU!')
+    print(termo, end=' → ')
+    termo += razao
+print('PAUSA')
 print('')
 
-
-
 maister = 0
-maiste = 1
-while maiste != 0:
-    maiste = int(input('Deseja ver mais termos? \nSe não digite 0 \nSe sim Quantos: '))
-    maister += maiste
+mais = 1
+cont = 10
+while mais != 0:
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+    print('')
+    maister += mais
     while c < 10 + maister:
-        print(primter, end=' → ')
-        primter += razao
+        print(termo, end=' → ')
+        termo += razao
         c += 1
+        cont += 1
+    if mais != 0:
+        print('PAUSA')
+        print('')
+
+print('Foram mostrados {} termos.'.format(cont))
+print('ENCERRANDO PROGRAMA...')
+sleep(2)
+print('PROGRAMA ENCERRADO')
