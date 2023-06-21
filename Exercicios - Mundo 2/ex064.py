b1a5238f492e425250a12c4e1ba97f938180534b
Allan <allandiascavalcante@gmail.com>
2023-06-20 21:12:32 -0300

    Update ex064.py

--- Exercicios - Mundo 2/ex064.py
+++ Exercicios - Mundo 2/ex064.py
@@ -1,8 +1,7 @@
-n = 0
-num = 0
+n = num = 0
 cont = -1
 while n != 999:
     cont += 1
     num += n
-    n = int(input('Digite um número: '))
+    n = int(input('Digite um número [999 para parar]: '))
 print('Foram digitados {} números \nA soma entre os números digitados é {}'.format(cont, num))
