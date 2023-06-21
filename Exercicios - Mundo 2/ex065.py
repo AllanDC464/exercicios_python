9ec8b79a721a0a47d853da576152d3cd1f2e4de5
Allan <allandiascavalcante@gmail.com>
2023-06-20 21:45:46 -0300

    Update ex065.py

--- Exercicios - Mundo 2/ex065.py
+++ Exercicios - Mundo 2/ex065.py
@@ -1,5 +1,4 @@
-num = 0
-cont = 0
+num = cont = 0
 resp = ''
 while resp in 'Ss':
     n = int(input('Digite um número: '))
@@ -7,13 +6,11 @@ while resp in 'Ss':
     num += n
     cont += 1
     if cont == 1:
-        numMA = n
-        numME = n
+        numMA = numME = n
     else:
         if n > numMA:
             numMA = n
         if n < numME:
             numME = n
-print('A média entre os valores digitados é de {}'.format(num/cont))
-print('O maior valor digitado foi {}'.format(numMA))
-print('O menor valor digitado foi {}'.format(numME))
+print('Você digitou \033[4;32m{}\033[m números e a média foi \033[4;32m{}\033[m'.format(cont, num/cont))
+print('O maior valor foi \033[4;32m{}\033[m e o menor foi \033[4;32m{}\033[m'.format(numMA, numME))
