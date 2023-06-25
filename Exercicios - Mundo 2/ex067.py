66d28de7779ad6782bbcaa5a9ef66dd7ff6f4d0f
Allan <allandiascavalcante@gmail.com>
2023-06-25 15:21:56 -0300

    Update ex067.py

--- Exercicios - Mundo 2/ex067.py
+++ Exercicios - Mundo 2/ex067.py
@@ -1,13 +1,10 @@
-tab = 0
 while True:
-    num = 0
     tab = int(input('Quer ver a tabuada de qual valor? '))
     if tab < 0:
         break
     print('▬' * 40)
-    while num != 10:
-        num += 1
-        print(f'{tab} x {num:2} = {tab * num}')
+    for c in range(1, 11):
+        print(f'{tab} x {c:2} = {tab * c}')
     print('▬' * 40)
 print('▬' * 40)
 print('PROGRAMA TABUADA ENCERRADO. Volte sempre!')
