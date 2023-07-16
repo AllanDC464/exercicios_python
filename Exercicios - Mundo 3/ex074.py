4e3a5936c6da8e24a2e0266069b06dc2e546da89
Allan <allandiascavalcante@gmail.com>
2023-07-16 14:36:10 -0300

    Update ex074.py

--- Exercicios - Mundo 3/ex074.py
+++ Exercicios - Mundo 3/ex074.py
@@ -1,6 +1,6 @@
 from random import randint
 
-numeroAleatorio = tuple(randint(1, 9) for c in range(1, 6))
+numeroAleatorio = tuple(randint(1, 10) for c in range(1, 6))
 
 print('Os valores sorteados foram:', end=' ')
 for c in range(0, len(numeroAleatorio)):
@@ -10,3 +10,13 @@ print('')
 numeroOrganizado = sorted(numeroAleatorio)
 print(f'O menor número sorteado foi {numeroOrganizado[0]}')
 print(f'O maior número sorteado foi {numeroOrganizado[-1]}')
+print('▬' * 40)
+
+numeros = (randint(1, 10), randint(1, 10), randint(1, 10),
+randint(1, 10), randint(1, 10))
+
+print('Os valores sorteados foram: ', end='')
+for n in numeros:
+    print(f'{n} ', end='')
+print(f'\nO menor valor sorteador foi {min(numeros)}')
+print(f'O maior valor sorteador foi {max(numeros)}')
