6383a28f21d8e0f7d1d632841371b4fb817a4f76
Allan <allandiascavalcante@gmail.com>
2023-07-16 15:45:01 -0300

    Update ex076.py

--- Exercicios - Mundo 3/ex076.py
+++ Exercicios - Mundo 3/ex076.py
@@ -2,7 +2,9 @@ print('▬' * 41)
 print('LISTAGEM DE PREÇOS'.center(41))
 print('▬' * 41)
 
-produto = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90, 'Estojo', 25.00, 'Tranferidor', 4.20, 'Compasso', 9.99, 'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.90)
+produto = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90,
+'Estojo', 25.00, 'Tranferidor', 4.20, 'Compasso', 9.99, 'Mochila',
+120.32, 'Canetas', 22.30, 'Livro', 34.90)
 
 np = 0
 vp = 1
