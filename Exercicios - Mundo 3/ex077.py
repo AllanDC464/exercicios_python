3e2fcd4ba7089a6b091334c49a44a27b7435c320
Allan <allandiascavalcante@gmail.com>
2023-07-16 15:50:57 -0300

    Update ex077.py

--- Exercicios - Mundo 3/ex077.py
+++ Exercicios - Mundo 3/ex077.py
@@ -1,8 +1,9 @@
-palavras = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS', 'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
+palavras = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO',
+'GRATIS', 'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO',
+'PROGRAMADOR', 'FUTURO')
 
 for c in range(0, len(palavras)):
-    print(' ')
-    print(f'Na palavra {palavras[c]} temos', end=' ')
+    print(f'\nNa palavra {palavras[c]} temos', end=' ')
     for i in list(palavras[c]):
         if i in 'AEIOU':
             print(i, end=' ')
