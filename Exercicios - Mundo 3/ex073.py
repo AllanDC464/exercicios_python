5014a1dce5127c38d8388578e99b380380fdd96b
Allan <allandiascavalcante@gmail.com>
2023-07-16 14:25:16 -0300

    Update ex073.py

--- Exercicios - Mundo 3/ex073.py
+++ Exercicios - Mundo 3/ex073.py
@@ -1,20 +1,25 @@
 tabelaDoBrasileirão = ('', 'Botafogo', 'Flamengo', 'Grêmio', 'Fluminense', 'Palmeiras', 'Bragantino', 'Fortaleza', 'São Paulo', 'Cruzeiro', 'Internacional', 'Athletico-PR', 'Atlético-MG', 'Santos', 'Cuiabá', 'Corinthians', 'Bahia', 'Goiás', 'Coritiba', 'Vasco da Gama', 'América-MG')
 
-print('Cinco Primeiros colocados na tabela do brasileirão')
+print('\033[32;4mTabela completa do brasileirão\033[m')
+for c in range(1, len(tabelaDoBrasileirão)):
+    print(f'{c:>2} {tabelaDoBrasileirão[c]}')
+print('')
+
+print('\033[32;4mCinco Primeiros colocados na tabela do brasileirão\033[m')
 for c in range(1, 6):
-    print(f'{c}', tabelaDoBrasileirão[c])
+    print(f'{c} {tabelaDoBrasileirão[c]}')
 print('')
 
-print('Quatro últimos colocados na tabela do brasileirão')
+print('\033[32;4mQuatro últimos colocados na tabela do brasileirão\033[m')
 for c in range(-4, 0):
-    print(f'{tabelaDoBrasileirão.index(tabelaDoBrasileirão[c])}', tabelaDoBrasileirão[c])
+    print(f'{tabelaDoBrasileirão.index(tabelaDoBrasileirão[c])} {tabelaDoBrasileirão[c]}')
 print('')
 
-print('Times em ordem alfabetica')
+print('\033[32;4mTimes em ordem alfabetica\033[m')
 tab = sorted(tabelaDoBrasileirão)
 for c in range(1, 21):
-    print(f'{tabelaDoBrasileirão.index(tab[c]):>2}', tab[c])
+    print(f'{tabelaDoBrasileirão.index(tab[c]):>2} {tab[c]}')
 print('')
 
-print('Em que posição esta o Athletico-PR')
-print('O Athletico-PR enta na posição ', tabelaDoBrasileirão.index('Athletico-PR'))
+print('\033[32;4mEm que posição esta o Athletico-PR\033[m')
+print(f'O Athletico-PR esta na  {tabelaDoBrasileirão.index("Athletico-PR")}ª posição')
