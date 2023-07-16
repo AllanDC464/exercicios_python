5624798004fbd77f7ee6c6690edfeb767f4256ee
Allan <allandiascavalcante@gmail.com>
2023-07-16 13:57:26 -0300

    Update ex072.py

--- Exercicios - Mundo 3/ex072.py
+++ Exercicios - Mundo 3/ex072.py
@@ -1,11 +1,17 @@
-contagemZeroAVinte = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Catorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
-
-número = int(input('Digite um número entre 0 e 20: '))
+contagemZeroAVinte = ('Zero', 'Um', 'Dois', 'Três', 'Quatro',
+'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze',
+'Treze', 'Catorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito',
+'Dezenove', 'Vinte')
 
 while True:
-    if número < 0 or número > 20:
-       print('Número invalido!')
-       número = int(input('Digite um número entre 0 e 20: '))
-    else:
-        print(f'O número \033[32;4m{número}\033[m por extenso é \033[32;4m{contagemZeroAVinte[número]}\033[m')
+    while True:
+        número = int(input('Digite um número entre 0 e 20: '))
+        if 0 <= número <= 20:
+            break
+        print('Número invalido!')
+    print(f'O número \033[32;4m{número}\033[m por extenso é \033[32;4m{contagemZeroAVinte[número]}\033[m')
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
         break
